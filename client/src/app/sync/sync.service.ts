303f1783e87c98e8191ba07f9dff9315247d405e
Ninette Adhikari <ninette.adhikari@gmail.com>
2021-06-11 12:45:23 +0200

    fix: Move time log in the query loop

--- client/src/app/sync/sync.service.ts
+++ client/src/app/sync/sync.service.ts
@@ -201,6 +201,7 @@ export class SyncService {
 
 // Sync Protocol 2 view indexer. This excludes views for SP1 and includes custom views from content developers.
   async indexViews() {
+    console.time('*** indexViews total time')
     const exclude = [
       'tangy-form/responsesLockedAndNotUploaded',
       'tangy-form/responsesUnLockedAndNotUploaded',
@@ -229,19 +230,20 @@ export class SyncService {
     let i = 0
     for (let row of result.rows) {
       if (row.doc.views) {
-        console.time('*** time indexing viewpath')
         for (let viewId in row.doc.views) {
           const viewPath = `${row.doc._id.replace('_design/', '')}/${viewId}`
           if (!exclude.includes(viewPath)) {
             console.log(`Indexing: ${viewPath}`)
+            console.time(`*** time indexing query ${viewPath}`)
             await db.query(viewPath, { limit: 1 })
+            console.timeEnd(`*** time indexing query ${viewPath}`)
           }
-        console.timeEnd('*** time indexing viewpath')
         }
       }
       this.syncMessage$.next({ message: `${window['t']('Optimizing data. Please wait...')} ${Math.round((i/result.rows.length)*100)}%` })
       i++
     }
+    console.timeEnd('*** indexViews total time')
   }
 
   async createSyncFormIndex(username:string = '') {
